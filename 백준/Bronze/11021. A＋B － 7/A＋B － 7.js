20f0c94575c7d6d3077eaf590db94f37648db529
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-05-16 22:40:23 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = fs.readFileSync('dev/stdin').toString().trim().split("\n")
+const count = parseInt(input[0]);
+for(let i = 1 ; i <= count ; i++){
+    const a = parseInt(input[i].split(" ")[0]);
+    const b = parseInt(input[i].split(" ")[1]);
+    const sum = a+b;
+    console.log(`Case #${i}: ${sum}`)
+}
