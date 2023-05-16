314ff81ccd0bd5be203d97236de869a1973841db
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-05-16 22:59:32 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split("\n")
+
+for(let i = 0 ; i <= input.length -2 ; i++){
+    const a = parseInt(input[i].split(" ")[0]);
+    const b = parseInt(input[i].split(" ")[1]);
+    console.log(a+b)
+    
+}
